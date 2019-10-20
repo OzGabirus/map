40b971747f25bedcc1f650050ee682d72fba42e3
Adriel Fabricio <adrielfabricios@gmail.com>
2019-10-20 06:59:17 -0300

    update maps

---
+++ assets/js/map.js
@@ -0,0 +1,183 @@
+var url = "http://10.3.121.243:3333/api/v1/geolocation";
+var marker_colors = {
+  0: "green",
+  1: "yellow",
+  2: "orange",
+  3: "red",
+  4: "purple",
+  5: "blue"
+};
+
+$(document).ready(function() {
+  $(".dropdown-trigger").dropdown({
+    hover: true,
+    coverTrigger: false
+  });
+  $("#filtro_btn").hover(function() {
+    $("#filtro").fadeIn();
+  });
+  $("#filtro").on("click", function() {
+    $("#filtro").fadeOut();
+  });
+  $("#dark_tema").on("click", function() {
+    refresh_map(dark_tema);
+    map.setOptions({ styles: myStyle });
+  });
+});
+
+function theme_map(lat, long, zoom, style, idElement) {
+  let map = new google.maps.Map(document.getElementById(idElement), {
+    center: { lat: lat, lng: long },
+    zoom: zoom,
+    styles: style
+  });
+  return map;
+}
+
+function refresh_map(name_tema) {
+  var map = theme_map(-34.397, 80.644, 10, dark_theme, "map");
+  var input = document.getElementById("search-box");
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+}
+
+async function fetch_data_api(url) {
+  var data_json = (await fetch(url)).json();
+  var coords = await data_json;
+  return coords;
+}
+
+async function initMap() {
+  // DOM envs
+  var map = theme_map(-34.397, 80.644, 10, dark_theme, "map");
+  var infoWindow = new google.maps.InfoWindow();
+  var geocoder = new google.maps.Geocoder();
+  var heatmap_data = [];
+  var input = document.getElementById("search-box");
+  var search_box = new google.maps.places.SearchBox(input);
+  var data_coordinates;
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      function(position) {
+        var pos = new google.maps.LatLng(
+          position.coords.latitude,
+          position.coords.longitude
+        );
+
+        // DEBUG      =============
+        geocoder.geocode({ latLng: pos }, function(results, status) {
+          json_address_info = results[0].address_components;
+        });
+        // END DEBUG  =============
+
+        infoWindow.setPosition(pos);
+        var marker = new google.maps.Marker({
+          position: pos,
+          map: map
+        });
+        map.setCenter(pos);
+      },
+      () => {
+        handleLocationError(true, infoWindow, map.getCenter());
+      }
+    );
+  } else {
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+
+  var data_coordinates = await fetch_data_api(url);
+  var markers = [],
+    info_windows = [];
+  for (let i = 0; i < 100; i++) {
+    let url = "http://maps.google.com/mapfiles/ms/icons/";
+    url += marker_colors[i % 6] + "-dot.png";
+    markers[i] = new google.maps.Marker({
+      position: new google.maps.LatLng(
+        data_coordinates[i].latitude,
+        data_coordinates[i].longitude
+      ),
+      map: map,
+      icon: {
+        url: url
+      }
+      // label: atributo_tabela
+    });
+    info_windows[i] = new google.maps.InfoWindow({
+      content:
+        '<div class="container">\
+      <div class="title center-align">Titulo</div>\
+      <hr />\
+      <div class="row">\
+        <div class="col-s12">BLA1: ...</div>\
+        <div class="col-s12">BLA2: ...</div>\
+        <div class="col-s12">BLA3: ...</div>\
+      </div>\
+    </div>'
+    });
+
+    markers[i].addListener("click", function() {
+      info_windows[i].open(map, markers[i]);
+    });
+  }
+
+  // heatmap
+  // var heatmap = new google.maps.visualization.HeatmapLayer({
+  //   data: wind_points
+  // });
+  // heatmap.setMap(map);
+
+  // listeners
+  map.addListener("click", function() {});
+  map.addListener("bounds_changed", function() {
+    search_box.setBounds(map.getBounds());
+  });
+  google.maps.event.addListener(search_box, "places_changed", function() {
+    search_box.set("map", null);
+    var places = search_box.getPlaces();
+    var bounds = new google.maps.LatLngBounds();
+    var i, place;
+    for (i = 0; (place = places[i]); i++) {
+      (function(place) {
+        var marker = new google.maps.Marker({
+          position: place.geometry.location
+        });
+        marker.bindTo("map", search_box, "map");
+        google.maps.event.addListener(marker, "map_changed", function() {
+          if (!this.getMap()) {
+            this.unbindAll();
+          }
+        });
+        bounds.extend(place.geometry.location);
+      })(place);
+    }
+    map.fitBounds(bounds);
+    search_box.set("map", map);
+    map.setZoom(Math.min(map.getZoom(), 12));
+    google.maps.event.addListener(marker, "click", function() {
+      if (infoWindow) {
+        infoWindow.close();
+      }
+      infoWindow = new google.maps.InfoWindow({ content: "dasdasd" });
+      infoWindow.open(map, marker);
+    });
+  });
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
+
+function addMarker(location, map) {
+  var marker = new google.maps.Marker({
+    position: location,
+    label: labels[labelIndex++ % labels.length],
+    map: map
+  });
+}
---
+++ assets/js/script.js
@@ -0,0 +1,120 @@
+var data = data_start("scatter");
+function data_start(type) {
+  var trace1 = {
+    x: [1, 2, 3, 4],
+    y: [10, 15, 13, 17],
+    type: type
+  };
+
+  var trace2 = {
+    x: [2, 3, 4, 5],
+    y: [16, 5, 11, 9],
+    type: type
+  };
+
+  var trace3 = {
+    x: [2, 5, 7, 5],
+    y: [16, 3, 8, 9],
+    type: type
+  };
+
+  var trace4 = {
+    x: [2, 5, 7, 5],
+    y: [66, 4, 12, 9],
+    type: type
+  };
+
+  return [trace1, trace2, trace3, trace4];
+}
+
+function data_start_mode(type) {
+  var trace1 = {
+    x: [1, 2, 3, 4],
+    y: [10, 15, 13, 17],
+    mode: type
+  };
+
+  var trace2 = {
+    x: [2, 3, 4, 5],
+    y: [16, 5, 11, 9],
+    mode: type
+  };
+
+  var trace3 = {
+    x: [2, 5, 7, 5],
+    y: [16, 3, 8, 9],
+    mode: type
+  };
+
+  var trace4 = {
+    x: [2, 5, 7, 5],
+    y: [66, 4, 12, 9],
+    mode: type
+  };
+
+  return [trace1, trace2, trace3, trace4];
+}
+
+/* GLOBAL SHIT*/
+
+$(document).ready(function() {
+  //START
+  for (let i = 0; i < data.length; i++) {
+    let graph =
+      '<div class="center" id="graph' +
+      i.toString() +
+      '"style="width:800px;height:400px;"></div>';
+    $(".graph_area").append(graph);
+    Plotly.newPlot("graph" + i.toString(), data);
+  }
+});
+
+$("#bar").on("click", function() {
+  remove_graph();
+  data = data_start("bar");
+  plot_graph();
+});
+
+$("#lines").on("click", function() {
+  remove_graph();
+  data = data_start("scatter");
+  plot_graph();
+});
+
+$("#dots").on("click", function() {
+  remove_graph();
+  data = data_start_mode("markers");
+  plot_graph();
+});
+
+function plot_graph(mode, type) {
+  for (let i = 0; i < data.length; i++) {
+    let graph =
+      '<div class="center" id="graph' +
+      i.toString() +
+      '"style="width:800px;height:400px;"></div>';
+    $(".graph_area").append(graph);
+    Plotly.newPlot("graph" + i.toString(), data);
+  }
+}
+
+function remove_graph() {
+  for (let i = 0; i < data.length; i++) {
+    $("#graph" + i.toString()).remove();
+  }
+}
+
+$("#project").on("click", function() {
+  remove_graph();
+  $(".project").fadeIn(100);
+});
+
+$("#enviar").on("click", function() {
+  $("#map_area").css("display", "none");
+  $("#graph_area").css("display", "block");
+});
+
+$("#voltar").on("click", function() {
+  $("#map_area").css("display", "block");
+  $("#graph_area").css("display", "none");
+});
